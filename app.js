33fdf7d8b8e148392ba3165a5230c50a7d6af38d
mysticBel <dnxbel@gmail.com>
2021-09-28 20:43:45 -0500

    first commit , adding a server with Express, + using  Nodemon

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require ('express');
+const app = express();  //executing and ready to listen any request
+//enables to access all properties and methods from 'Express' package
+// default url, callback function
+app.get('/' , (req,res) => {
+    //res.end()
+    res.send('hey! hello from Express :P');
+});
+
+const port = 8080;
+app.listen(port, () => {
+     console.log(`A Node Js API is listening on port: ${port}`);
+});
+
+
+//----------------------------------
+// const http = require (`http`);
+
+// const server = http.createServer((req, res) =>{
+//     res.end('hello world from node js , this is an update using nodemon');
+// });
+
+// server.listen(3000);
+//port 3000 , localhost:3000
+//----------------------------------
+// console.log('hello world :3');
+
+// const helper = require ('./helper');
+//  const { sum } = require ('./helper');
+
+//putting the arguments
+// const total = sum(10,200);
+// console.log('TOTAL:',total);
+
---
+++ helper.js
@@ -0,0 +1,20 @@
+// console.log('PROCESS:', process)
+
+// function sum (a,b) {
+//     return a + b;
+// }
+
+// // arrow functions
+// const sum = (a, b) => {
+//     return a + b;
+// };
+
+// // one line statement
+// const sum = (a, b) => a + b;
+
+// module.exports = {
+//     sum
+// };
+
+// exports one line statement
+exports.sum = (a, b) => a + b;
