9df5bb52683b5b97bc691f6fc624a9e2c570ef20
mysticBel <dnxbel@gmail.com>
2021-10-01 17:41:25 -0500

    changing to res.json in postController.js to use Postman

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,5 +1,13 @@
 //extracting the logic from '../routes/post.js'
 exports.getPosts = (req,res) => {
     
-    res.send('hey! hello from Express :P');
+    // res.send('hey! hello from Express :P');
+
+    // using JSON
+    res.json({
+        posts: [{ title : "First post"},{ title : "Second post"}]
+    });
 };
+
+// TO see it better in the browser, install JSON viewer(Google Extensions)
+// To handle API , we will use Postman.
