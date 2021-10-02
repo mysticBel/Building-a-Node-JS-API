2c909b51dd9a3847afc1be99f54cf514a8016093
mysticBel <dnxbel@gmail.com>
2021-10-01 19:17:49 -0500

    ＼(＾O＾)/  database from MongoDB is connected !

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 // bring in routes using object destructuring :P
 
 const express = require ('express');
-const morgan = require('morgan'); 
 const app = express();  //executing and ready to listen any request
 //enables to access all properties and methods from 'Express' package
 // default url, callback function
+const morgan = require('morgan'); 
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+// db :
+mongoose.connect(
+    'mongodb+srv://ElmoOnFire666:elmo666@nodeapi.rd96d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    {useNewUrlParser:true}
+    )
+.then(() => console.log('DB from MongoDB connected succesfully! :D '));
+// if there's an error in database
+mongoose.connection.on('error', err => {
+    console.log(`DB connection error: ${err.message}`);
+});
+
+
 const postRoutes = require('./routes/post.js') // postRoutes now works as a middleware
 //middleware
 app.use(morgan('dev'));
@@ -12,7 +27,9 @@ app.use(morgan('dev'));
 app.use('/', postRoutes);
 
 
-const port = 8080;
+// const port = 8080; Let's use dotenv :
+const port = process.env.PORT || 8080 ;
+
 app.listen(port, () => {
      console.log(`A Node Js API is listening on port: ${port}`);
 });
